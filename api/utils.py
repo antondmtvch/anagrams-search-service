a0045857d2b09d6273a93a741544feb646b3b6f0
Anton Teplyakov <antondmtvch@gmail.com>
2020-03-15 12:52:55 +0300

    utils.py added

---
+++ api/utils.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from typing import Union
+
+import ujson
+from aiohttp import web
+
+
+def json_response(data: Union[list, None], **kwargs) -> web.Response:
+    kwargs.setdefault('headers', {'content-type': 'application/json'})
+    return web.Response(body=ujson.dumps(data, ensure_ascii=False), **kwargs)
+
+
+def sort(word: str) -> Union[str, None]:
+    return ''.join(sorted(word.lower().strip()))
+
+
+def get_anagrams_dict(words: list) -> defaultdict:
+    dd = defaultdict(set)
+    for w in words:
+        key = sort(w)
+        dd[key].add(w)
+    return dd
