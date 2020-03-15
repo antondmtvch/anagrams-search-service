c2d7f8b56d0477acf4b8e4ddc33908b71f975304
Anton Teplyakov <antondmtvch@gmail.com>
2020-03-15 17:34:13 +0300

    test db added

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,7 @@ from settings import load_settings
 
 @pytest.fixture
 async def cli(loop, aiohttp_client):
-    app = await init_app(load_settings())
+    settings = load_settings()
+    db = settings['redis']['REDIS_TEST_DB']
+    app = await init_app(settings, db)
     return await aiohttp_client(app)
